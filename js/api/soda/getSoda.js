d5cc6397bdba8882b8ba6b17d2e92624d561f45d
Stephen Lewis <st.rayis1085@gmail.com>
2026-02-17 14:20:07 -0500

    fix: removed comments not needed, console.logs, and converted delete btn t o js

--- js/api/soda/getSoda.js
+++ js/api/soda/getSoda.js
@@ -21,8 +21,8 @@
   const serve = document.getElementById("served");
 
   // Delete Soda button
-  const $deleteBtn = $("#deleteSoda");
-  // const deleteBtn = document.getElementById("deleteSoda");
+  // const $deleteBtn = $("#deleteSoda");
+  const deleteBtn = document.getElementById("deleteSoda");
 
   // Make soda ajax request
   $.ajax({
@@ -37,7 +37,10 @@
         renderSoda(res.soda);
       }
     })
-    .catch((err) => $("section").text("Please choose a soda"));
+    .catch((err) => {
+      $("section").text("Please choose a soda");
+      console.log("errorCatch for GET\n", err);
+    });
 
   const renderSoda = ({ name, brand, fizziness, rating, served }) => {
     // console.log("inside render\n", served);
@@ -52,16 +55,7 @@
     $brand.text(brand);
     $fizziness.text(fizziness);
     $rating.text(rating);
-    serve.textContent = `${served} right here`;
-
-    // Check value of soda and provide conditions to serve or stop serving
-    // serveSoda.textContent = served ? "Stop serving soda" : "Serve soda";
-    // if (served === false) {
-    //   serveSoda.textContent = "Serve soda";
-    // } else {
-    //   serveSoda.textContent = "Stop serving soda";
-    // }
-    // Assinged served value to window object
+    serve.textContent = `${served}`;
 
     !served
       ? (serveSoda.textContent = "Serve soda")
@@ -74,10 +68,8 @@
   function updateSoda() {
     // Get value of serving soda
     const serving = window.served;
-    console.log("inside before Ajax\n", serving);
     // Update the serving object by toggling the value between true or false
     const updateValue = serving ? false : true;
-    console.log("update value\n", typeof updateValue);
 
     $.ajax({
       type: "PUT",
@@ -85,24 +77,17 @@
       data: { serving: updateValue },
     })
       .done((res) => {
-        // res is string
         const { serving } = res;
-        console.log("response\n", res);
-        console.log("inside update\n", serving);
-
         window.served = updateValue;
-        // goes from true to false or false to true but gets stuck in that state??
         if (updateValue) {
-          console.log("if true:", updateValue);
           serve.textContent = `${serving}`;
           serveSoda.textContent = "Stop serving soda";
         } else {
-          console.log("else false:", serving);
           serve.textContent = `${serving}`;
           serveSoda.textContent = "Serve soda";
         }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log("errorCatch in PUT\n", err));
   }
 
   // Add event listener to serve soda button
@@ -117,9 +102,9 @@
         alert("Soda successfully deleted!");
         window.location = "./sodas.html";
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log("errorCatch in DELETE\n", err));
   }
 
   // Add event listener to delete soda button
-  $deleteBtn.on("click", deleteSoda);
+  deleteBtn.addEventListener("click", deleteSoda);
 })();
