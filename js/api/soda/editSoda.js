ba68ac5213dae0ce0fd1ce736de448b8946df5e2
Stephen Lewis <st.rayis1085@gmail.com>
2026-02-17 23:10:10 -0500

    fix: removed commented out code and console.logs

--- js/api/soda/editSoda.js
+++ js/api/soda/editSoda.js
@@ -10,7 +10,6 @@
 
   const sodaApi = "http://localhost:3000/soda/" + sodaID;
 
-  // const $editBtn = $("#editSoda");
   const editBtn = document.getElementById("editSoda");
 
   const name = document.getElementById("name");
@@ -18,14 +17,13 @@
   const fizziness = document.getElementById("fizziness");
   const rating = document.getElementById("rating");
 
-  // Assign a click event for edit button
   editBtn.addEventListener("click", function () {
     // Get the existing value of the elements
     const nameVal = name.textContent;
     const brandVal = brand.textContent;
     const fizzinessVal = fizziness.textContent;
     const ratingVal = rating.textContent;
-    // Assign those previous values to these input for editing
+
     name.innerHTML = `
             <input name="name" class="edit-input" type='text' value='${nameVal}' />`;
     brand.innerHTML = `
@@ -35,29 +33,24 @@
     rating.innerHTML = `
             <input name="rating" class="edit-input" type='number' value='${ratingVal}' />`;
 
-    // Convert 'this' button to 'Save'
     this.textContent = "Save";
-    //Unbind this button from the previous click fuction
     this.removeEventListener("click", arguments.callee);
-    // Assign this button to save details function for ajax request
     this.addEventListener("click", saveDetails);
   });
 
   function saveDetails() {
-    // Get input elements for editing/saving new values
     const name = document.querySelector('input[name="name"]').value;
     const brand = document.querySelector('input[name="brand"]').value;
     const fizziness = document.querySelector('input[name="fizziness"]').value;
     const rating = document.querySelector('input[name="rating"]').value;
-    // Create a new soda object
+
     const sodaObj = {
       name,
       brand,
       fizziness,
       taste_rating: rating,
     };
-    console.log("sodaObj before fetch\n", sodaObj);
-    // Send a PUT request to update the document in mongo
+
     fetch(sodaApi, {
       method: "PUT",
       headers: {
@@ -77,20 +70,5 @@
         console.log(err);
         alert("Oops, something went wrong!");
       });
-
-    // $.ajax({
-    //   type: "PUT",
-    //   url: sodaApi,
-    //   data: sodaObj,
-    // })
-    //   .done((res) => {
-    //     // Once updated successfully, render an alert
-    //     alert("Updated soda!");
-    //     location.reload();
-    //   })
-    //   .catch((err) => {
-    //     alert("Oops, something went wrong!");
-    //     console.log(err);
-    //   });
   }
 })();
