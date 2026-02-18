e44659aed8215ffc6f652fc8df51108c5012e533
Stephen Lewis <st.rayis1085@gmail.com>
2026-02-17 22:27:26 -0500

    fix: made DOM varibles global for access in both functions

--- js/api/soda/editSoda.js
+++ js/api/soda/editSoda.js
@@ -10,49 +10,51 @@
 
   const sodaApi = "http://localhost:3000/soda/" + sodaID;
 
-  const $editBtn = $("#editSoda");
-  // Assign a click event for edit button
-  $editBtn.on("click", function () {
-    // Convert 'this' button to 'Save'
-    $(this).text("Save");
-    // Declare all elements of sodas info
-    const $name = $("#name");
-    const $brand = $("#brand");
-    const $fizziness = $("#fizziness");
-    const $rating = $("#rating");
-    // Get the existing value of the elements
-    const nameVal = $name.text();
-    const brandVal = $brand.text();
-    const fizzinessVal = $fizziness.text();
-    const ratingVal = $rating.text();
-    // Assign those previous values to these input for editing
-    $name.html(`
-            <input name="name" class="edit-input" type='text' value='${nameVal}' />`);
-    $brand.html(`
-            <input name="brand" class="edit-input" type='text' value='${brandVal}' />`);
-    $fizziness.html(`
-            <input name="fizziness" class="edit-input" type='number' value='${fizzinessVal}' />`);
-    $rating.html(`
-            <input name="rating" class="edit-input" type='number' value='${ratingVal}' />`);
+  // const $editBtn = $("#editSoda");
+  const editBtn = document.getElementById("editSoda");
 
+  const name = document.getElementById("name");
+  const brand = document.getElementById("brand");
+  const fizziness = document.getElementById("fizziness");
+  const rating = document.getElementById("rating");
+
+  // Assign a click event for edit button
+  editBtn.addEventListener("click", function () {
+    // Get the existing value of the elements
+    const nameVal = name.textContent;
+    const brandVal = brand.textContent;
+    const fizzinessVal = fizziness.textContent;
+    const ratingVal = rating.textContent;
+    // Assign those previous values to these input for editing
+    name.innerHTML = `
+            <input name="name" class="edit-input" type='text' value='${nameVal}' />`;
+    brand.innerHTML = `
+            <input name="brand" class="edit-input" type='text' value='${brandVal}' />`;
+    fizziness.innerHTML = `
+            <input name="fizziness" class="edit-input" type='number' value='${fizzinessVal}' />`;
+    rating.innerHTML = `
+            <input name="rating" class="edit-input" type='number' value='${ratingVal}' />`;
+
+    // Convert 'this' button to 'Save'
+    this.textContent = "Save";
     //Unbind this button from the previous click fuction
-    $(this).unbind();
+    this.removeEventListener("click", arguments.callee);
     // Assign this button to save details function for ajax request
-    $(this).on("click", saveDetails);
+    this.addEventListener("click", saveDetails);
   });
 
   function saveDetails() {
     // Get input elements for editing/saving new values
-    const $name = $('input[name="name"]').val();
-    const $brand = $('input[name="brand"]').val();
-    const $fizziness = $('input[name="fizziness"]').val();
-    const $rating = $('input[name="rating"]').val();
+    const nameVal = name.textContent;
+    const brandVal = brand.textContent;
+    const fizzinessVal = fizziness.textContent;
+    const ratingVal = rating.textContent;
     // Create a new soda object
     const sodaObj = {
-      name: $name,
-      brand: $brand,
-      fizziness: $fizziness,
-      taste_rating: $rating,
+      name: nameVal,
+      brand: brandVal,
+      fizziness: fizzinessVal,
+      taste_rating: ratingVal,
     };
     // Send a PUT request to update the document in mongo
     $.ajax({
