033eb055e609f567a2eac4d9573059d866d232f0
Stephen Lewis <st.rayis1085@gmail.com>
2026-03-13 11:18:18 -0400

    changed server to .ts

---
+++ server.ts
@@ -0,0 +1,38 @@
+import config from "#config";
+import express, { ErrorRequestHandler } from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import sodaRouter from "#Routes/Soda";
+import dinerRouter from "#Routes/Diner";
+import { connect } from "#Connect";
+
+const app = express();
+
+let initalized = false;
+app.use((_, res, next) => {
+  if (!initalized) {
+    res.clearCookie("diner");
+    res.clearCookie("soda");
+    initalized = true;
+  }
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static("../SodaDiner"));
+
+app.use("/soda(s)?", sodaRouter);
+app.use("/diner(s)?", dinerRouter);
+
+connect().then(() => {
+  app.listen(config.port, () => {
+    console.log(`@ port ${config.port}`);
+  });
+});
+
+const handler: ErrorRequestHandler = (err, req, res, next) => {
+  console.log(`${err}`);
+};
+app.use(handler);
