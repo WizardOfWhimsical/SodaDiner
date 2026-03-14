93df4fee01d73b17656bff7e4d5ea52e4e65a435
Stephen Lewis <st.rayis1085@gmail.com>
2026-03-14 13:06:06 -0400

    feat: made basic fetch helper in ts

--- src/helpers/api-fetch.ts
+++ src/helpers/api-fetch.ts
@@ -1,14 +1,29 @@
-export default async function fetchBase(pathName, options) {
+export default async function fetchBase<T>(
+  pathName: string,
+  options?: RequestInit,
+) {
   try {
     const response = await fetch(pathName, options);
     if (!response.ok) {
-      const err = response.json();
+      const err = await response.json();
       throw new Error("Base-Fetch Error\n", err);
     }
-    const data = response.json();
+    const data: T = await response.json();
     return { data, error: null };
   } catch (error) {
     console.log("Base Catch Error");
     return { data: null, error };
   }
 }
+
+interface Todo {
+  id: string;
+  title: string;
+}
+
+const user = await fetchBase<{ id: string }>("user");
+
+const todoList = await fetchBase<Todo[]>("todo");
+todoList.data?.map((todo) => {
+  console.log(todo.title);
+});
