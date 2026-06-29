b809ea2e69bed8a467b478ebeab90bc3ccdf625c
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-29 19:52:26 -0400

    feat: baseFetch for get sodas and renderSoda called

--- client/js/api/soda/getSoda.ts
+++ client/js/api/soda/getSoda.ts
@@ -1,4 +1,16 @@
 import { getElById } from "../../../src/helpers/getElement";
+import fetchBase from "../../../src/helpers/api-fetch";
+
+interface Soda {
+  /**
+   * then name of the soda
+   */
+  name: string;
+  brand: string;
+  fizziness: number;
+  rating: number;
+  served: boolean;
+}
 
 const cookies = document.cookie;
 
@@ -13,4 +25,15 @@ const apiServerUpdateSodan = "/api/soda/updateSoda/" + sodaId;
 const serveSoda = getElById("serveSoda");
 const deleteBtn = getElById("deleteSoda");
 const served = getElById("served");
-const section = document.getElementsByName("section");
+const section = document.querySelector("section") as HTMLElement;
+
+section.textContent = "Please choose a soda";
+
+const { data, error } = await fetchBase<Soda>(sodaApi);
+if (error) {
+  console.log(error.message, { error });
+  alert(`Opps, something went wrong\ncheck the logs`);
+}
+if (data) {
+  renderSoda(data.soda);
+}
