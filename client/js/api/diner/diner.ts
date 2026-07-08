f2ee0671a24b7a43920a585664ed8eba09946609
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-07-08 17:27:15 -0400

    feat: getSodas() created. Interface for Sodas and Diner created

--- client/js/api/diner/diner.ts
+++ client/js/api/diner/diner.ts
@@ -1,9 +1,38 @@
 import fetchBase from "../../../src/helpers/api-fetch";
 import { getElById } from "../../../src/helpers/getElement";
 
+interface Soda {
+  _id: string;
+  name: string;
+  brand: string;
+  fizziness: number;
+  rating: number;
+  served: boolean;
+}
+
+interface Diner {
+  name: string;
+  location: string;
+  _id: string;
+  sodas: Soda[];
+}
+
 const dinerForm = document.getElementById("diner-form") as HTMLFormElement;
 const sodaContainer = getElById("soda-container");
 
 const apiServerDiners = "/api/diners";
 const apiServerDiner = "/api/diner";
 const apiServerSoda = "/api/sodas/serving";
+
+async function getSodas() {
+  const { data, error } = await fetchBase<{ sodas: Soda[] }>(apiServerSoda);
+
+  if (error) {
+    alert("Something went wrong, go check the logs");
+    console.log("error from ServerSoda\n", error);
+  }
+
+  if (data) {
+    renderOptionElements(data);
+  }
+}
