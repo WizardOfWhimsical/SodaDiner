d4deee25e9abfff5f6a39a59c0c82f83f8c84530
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-07-20 17:28:43 -0400

    feat: stub for edit btn event listener

--- client/js/api/diner/editDiner.ts
+++ client/js/api/diner/editDiner.ts
@@ -1,5 +1,9 @@
 import fetchBase from "../../../src/helpers/api-fetch";
-import { getElById } from "../../../src/helpers/getElement";
+import {
+  getElById,
+  getButtonElById,
+  getInputElByName,
+} from "../../../src/helpers/getElement";
 
 const cookies = document.cookie;
 
@@ -9,3 +13,11 @@ const dinerID = cookies
   ?.split("=")[1];
 
 const dinerApi = "/api/diner/" + dinerID;
+
+const editBtn = getButtonElById("editDiner");
+
+editBtn.addEventListener("click", function switchMaterial() {
+  const name = getInputElByName("name");
+  const location = getInputElByName("location");
+  const sodaCont = getElById("sodas");
+});
